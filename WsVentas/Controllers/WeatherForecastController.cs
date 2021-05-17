1f672fa6433a7dafe0fab7db0926a357f7c65a5e
LeiscarT <leiscartp29@gmail.com>
2021-05-17 15:50:03 -0400

    Initial project Requirements

---
+++ WsVentas/Controllers/WeatherForecastController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WsVentas.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            List<WeatherForecast> lst = new List<WeatherForecast>();
+            lst.Add(new WeatherForecast() { Id = 1, Nombre = "Leiscar Trinidad" });
+            lst.Add(new WeatherForecast() { Id = 2, Nombre = "Raciel Peralta" });
+            lst.Add(new WeatherForecast() { Id = 3, Nombre = "Noe Trinidad" });
+
+            return lst;
+        }
+    }
+}
---
+++ WsVentas/WeatherForecast.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WsVentas
+{
+    public class WeatherForecast
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+    }
+}
